b9e03c5d8109d952d1b36c4b15a6dc8d9c965e72
GnanaDeepthi <36473596+GnanaDeepthi@users.noreply.github.com>
2018-02-20 08:35:17 +0530

    Update calculator.component.ts

--- calculator/src/app/calculator/calculator.component.ts
+++ calculator/src/app/calculator/calculator.component.ts
@@ -1,590 +1,382 @@
-import { Component, OnInit, Output, EventEmitter, ViewEncapsulation }
-  from '@angular/core';
+import { Component, OnInit , Output, EventEmitter,ViewEncapsulation} 
+from '@angular/core';
 
 
 
 @Component({
 
-  selector: 'app-calculator',
+selector: 'app-calculator',
 
-  templateUrl: './calculator.component.html',
+templateUrl: './calculator.component.html',
 
-  styleUrls: ['./calculator.component.css'],
-  encapsulation: ViewEncapsulation.Emulated,
+styleUrls: ['./calculator.component.css'],
+encapsulation: ViewEncapsulation.Emulated,
 
 })
 
 export class CalculatorComponent
-  implements OnInit {
+implements OnInit {
 
-  @Output() click = new EventEmitter();
+ @Output() click = new EventEmitter();
 
-  constructor() {
-
-  }
-
-
-
-  ngOnInit() {
-    this.click.emit(this.output);
-  }
-
-  public btn;
+constructor() {
+  
+ }
 
 
-  public newNumber: boolean = true;
 
-  public pastOperation = null;
+ngOnInit() {
+this.click.emit(this.output);
+}
 
-  public presentTotal = 0;
-
-  public pastTotal = null;
-
-  public lastOperation = null;
-
-  public symbol = "";
-
-  public ADD =
-  "adding";
-
-  public SUBTRACT =
-  "subtracting";
-
-  public MULTIPLY = "multiply";
-
-  public addSymbol =
-  "+";
-
-  public subtractSymbol =
-  "-";
-
-  public multiplySymbol = "*";
-
-  public DIVIDE = 'divide';
-
-  public divisionSymbol = "/";
-
-  public MODULO = 'modulo';
-
-  public moduloSymbol = "%";
-  public POW = 'pow';
-
-  public powSymbol = "^";
-  public ROOT = 'root';
-
-  public rootSymbol = "sqrt";
-  public SQUARE = 'square';
-
-  public sqrSymbol = "square";
-  public CUBE = 'cube';
-
-  public cubeSymbol = "cube";
-  public output = '0';
-
-  public lastValue = null;
-  public value1;
-  public token;
-  public toSqrt;
-  clicked(btn) {
-    this.click.emit(this.output);
-    this.btn = btn;
+public btn;
 
 
-    if (this.newNumber) {
+public newNumber:boolean=true;
 
-      this.output = btn;
+public pastTotalOperation=null;
+
+public pastTotal=0;
+
+public presentTotal=null;
+
+public lastOperation=null;
+
+public symbol="";
+
+public ADD = 
+"adding";
+
+public SUBTRACT = 
+"subtracting";
+
+public MULTIPLY="multiply";
+
+public addSymbol= 
+"+";
+
+public subtractSymbol = 
+"-";
+
+public multiplySymbol="*";
+
+public DIVIDE='divide';
+
+public divisionSymbol="/";
+
+public MODULO='modulo';
+
+public moduloSymbol="%";
+public POW='pow';
+
+public powSymbol="^";
+public ROOT='root';
+
+public rootSymbol="root";
+public SQUARE='square';
+
+public sqrSymbol="sqrt";
+public CUBE='cube';
+
+public cubeSymbol="cube";
+public output='0';
+
+public token;
+public toSqrt;
+clicked(btn){
+this.click.emit(this.output);
+this.btn=btn;
 
 
-      this.newNumber =
-        false;
+if(this.newNumber) {
 
+this.output=btn;
+this.newNumber = false;
+} else {
+this.output=String(this.output)+String(btn);
 
-    } else {
+}
 
+this.presentTotal = this.toNumber(this.output);
 
-      this.output = String(this.output) + String(btn);
+}
+calculation(operation){
+this.click.emit(this.output);
+this.token=operation;
+this.operations();
+}
 
-    }
+operations(){
+this.click.emit(this.output);
+switch(this.presentTotal!=null) {
 
-    this.pastTotal =
-      this.toNumber(this.output);
-    //alert(this.pastTotal);
+case (this.pastTotal &&this.pastTotalOperation == this.ADD ) :{
 
-  }
-  calculation(operation) {
-    this.click.emit(this.output);
-    this.token = operation;
-    this.operations();
-  }
-
-  operations() {
-    this.click.emit(this.output);
-
-    switch (this.pastTotal!=0) {
-      
-      case (this.presentTotal && this.pastOperation ==
-        this.ADD): {
-          
-          this.presentTotal+=
-            this.pastTotal;
-          break;
-        }
-      case (this.presentTotal &&
-        this.pastOperation ==
-        this.SUBTRACT): {
-
-          this.presentTotal-=
-            this.pastTotal;
-          break;
-        }
-      case (this.presentTotal &&
-        this.pastOperation ==
-        this.MULTIPLY): {
-
-          this.presentTotal*=
-            this.pastTotal;
+this.pastTotal += this.presentTotal;
 break;
-        }
-      case (this.presentTotal &&
-        this.pastOperation ==
-        this.DIVIDE): {
 
-          this.presentTotal/=
-            this.pastTotal;
+} case (this.pastTotal &&this.pastTotalOperation == this.SUBTRACT) :{
+
+this.pastTotal -= this.presentTotal;
 break;
-        }
-      case (this.presentTotal &&
-        this.pastOperation ==
-        this.MODULO): {
+}
 
-          this.presentTotal%=
-            this.pastTotal;
+case (this.pastTotal &&this.pastTotalOperation == this.MULTIPLY) :{
+
+this.pastTotal *= this.presentTotal;
 break;
-        }
-      case (this.presentTotal &&
-        this.pastOperation ==
-        this.ROOT): {
+}
 
-          this.presentTotal = Math.sqrt(this.presentTotal);
-          break;
-        }
-      case (this.presentTotal &&
-        this.pastOperation ==
-        this.POW): {
+case (this.pastTotal &&this.pastTotalOperation == this.DIVIDE) :{
 
-          this.presentTotal = Math.pow(this.presentTotal, this.pastTotal);
-          break;
-
-        }
-      case (this.presentTotal &&
-        this.pastOperation ==
-        this.SQUARE): {
-
-          this.presentTotal = Math.pow(this.presentTotal, 2);
+this.pastTotal /= this.presentTotal;
 break;
-        }
-      case (this.presentTotal &&
-        this.pastOperation ==
-        this.CUBE): {
+}
 
-          this.presentTotal = Math.pow(this.presentTotal, 3);
-break;}
+case (this.pastTotal &&this.pastTotalOperation == this.MODULO) :{
+
+this.pastTotal %= this.presentTotal;
+break;
+
+}
+case (this.pastTotal &&this.pastTotalOperation == this.ROOT) :{
+
+this.pastTotal=Math.sqrt(this.pastTotal);
+break;
+}
+case (this.pastTotal &&this.pastTotalOperation == this.POW) :{
+
+this.pastTotal = Math.pow(this.pastTotal,this.presentTotal);
+break;
+}
+case (this.pastTotal &&this.pastTotalOperation == this.SQUARE) :{
+
+this.pastTotal = Math.pow(this.pastTotal,2);
+break;
+}
+case (this.pastTotal &&this.pastTotalOperation == this.CUBE) :{
+
+this.pastTotal = Math.pow(this.pastTotal,3);
+break;
+}
 default:{
-this.presentTotal =this.pastTotal;
 
-        }
+this.pastTotal = this.presentTotal;
 
-    }
+}
+
+} 
 
 this.setsymbol(this.token);
 
-    this.setOutput(String(this.presentTotal));
+this.setOutput(String(this.pastTotal));
 
-    this.pastOperation =
-      this.token;
+this.pastTotalOperation = this.token;
 
-    this.newNumber =
-      true;
+this.newNumber = true;
 
-    this.pastTotal =
-      null;
+this.presentTotal = null;
 
-  }
+}
 
-  setsymbol(operation) {
-    this.click.emit(this.output);
-    if (operation ==
-      this.ADD) {
+setsymbol(operation) {
+this.click.emit(this.output);
+switch(operation!=null){
+case (operation == this.ADD): {
 
-      this.symbol =
-        this.addSymbol;
+this.symbol = this.addSymbol;
+break;
 
-    } else if (operation ==
-      this.SUBTRACT) {
+} case (operation ==this.SUBTRACT): {
 
-      this.symbol =
-        this.subtractSymbol;
+this.symbol = this.subtractSymbol;
+break;
 
-    } else if (operation ==
-      this.MULTIPLY) {
+} case(operation ==this.MULTIPLY) :{
 
-      this.symbol =
-        this.multiplySymbol;
+this.symbol = this.multiplySymbol;
+break;
+} 
 
-    }
+case (operation ==this.DIVIDE): {
 
-    else if (operation ==
-      this.DIVIDE) {
+this.symbol = this.divisionSymbol;
+break;
+} 
 
-      this.symbol =
-        this.divisionSymbol;
+case (operation ==this.MODULO): {
 
-    }
+this.symbol = this.moduloSymbol;
+break;
 
-    else if (operation ==
-      this.MODULO) {
+} 
+case (operation ==this.POW): {
 
-      this.symbol =
-        this.moduloSymbol;
+this.symbol = this.powSymbol;
+break;
+} 
+case (operation ==this.ROOT): {
 
-    }
-    else if (operation ==
-      this.POW) {
+this.symbol = this.rootSymbol;
+break;
+} 
+case (operation ==
+this.SQUARE): {
 
-      this.symbol =
-        this.powSymbol;
+this.symbol = this.sqrSymbol;
+break;
+} 
+case (operation ==this.CUBE): {
 
-    }
-    else if (operation ==
-      this.ROOT) {
+this.symbol = this.cubeSymbol;
+break;
+} 
 
-      this.symbol =
-        this.rootSymbol;
+default: {
 
-    }
-    else if (operation ==
-      this.SQUARE) {
+this.symbol = "";
+break;
 
-      this.symbol =
-        this.sqrSymbol;
+}
+}
+}
 
-    }
-    else if (operation ==
-      this.CUBE) {
+calculate() {
+this.click.emit(this.output);
 
-      this.symbol =
-        this.cubeSymbol;
+if(!this.newNumber) {
 
-    }
+this.presentTotal =this.toNumber(this.output);
 
-    else {
+} 
+switch(this.pastTotalOperation!=null){
+case (this.pastTotalOperation ==this.ADD): {
 
-      this.symbol =
-        "";
+this.pastTotal += this.presentTotal;
 
-    }
+this.lastOperation = this.ADD;
+break;
 
-  }
+} case (this.pastTotalOperation ==this.SUBTRACT) :{
 
-  calculate() {
-    this.click.emit(this.output);
+this.pastTotal -= this.presentTotal;
 
-    if (!this.newNumber) {
+break;
 
-      this.pastTotal = this.toNumber(this.output);
+}
 
-      this.lastValue =
-        this.pastTotal;
+case (this.pastTotalOperation ==this.MULTIPLY): {
 
-    }
+this.pastTotal *= this.presentTotal;
 
-    if (this.pastOperation == this.ADD) {
+break;
 
-      this.presentTotal +=
-        this.pastTotal;
+} 
 
-      this.lastOperation =
-        this.ADD;
+case (this.pastTotalOperation ==this.DIVIDE) :{
 
-    } else if (this.pastOperation == this.SUBTRACT) {
+this.pastTotal /= this.presentTotal;
+break;
 
-      this.presentTotal -=
-        this.pastTotal;
 
-      this.lastOperation =
-        this.SUBTRACT;
+}
 
-    }
+case (this.pastTotalOperation ==this.MODULO) :{
 
-    else if (this.pastOperation == this.MULTIPLY) {
+this.pastTotal %= this.presentTotal;
 
-      this.presentTotal *=
-        this.pastTotal;
+break;
 
-      this.lastOperation =
-        this.MULTIPLY;
+}
+case (this.pastTotalOperation ==this.POW) :{
 
-    }
+this.pastTotal = Math.pow(this.pastTotal,this.presentTotal);
 
-    else if (this.pastOperation == this.DIVIDE) {
+break;
 
-      this.presentTotal /=
-        this.pastTotal;
 
-      this.lastOperation =
-        this.DIVIDE;
+}
+case (this.pastTotalOperation ==this.ROOT): {
+this.pastTotal=Math.sqrt(this.pastTotal);
+break;
+}
+case (this.pastTotalOperation ==this.SQUARE): {
 
-    }
+this.pastTotal = Math.pow(this.pastTotal,2);
 
-    else if (this.pastOperation == this.MODULO) {
+break;
 
-      this.presentTotal %=
-        this.pastTotal;
 
-      this.lastOperation =
-        this.MODULO;
+}
+case (this.pastTotalOperation ==this.CUBE): {
 
-    }
-    else if (this.pastOperation == this.POW) {
+this.pastTotal = Math.pow(this.pastTotal,3);
+break;
 
-      this.presentTotal = Math.pow(this.presentTotal, this.pastTotal);
 
 
-      this.lastOperation =
-        this.POW;
+}
+}
+this.setOutput(this.pastTotal);
 
-    }
-    else if (this.pastOperation == this.ROOT) {
-      this.presentTotal = Math.sqrt(this.presentTotal);
-      this.lastOperation =
-        this.ROOT;
+this.setsymbol("");
 
-    }
-    else if (this.pastOperation == this.SQUARE) {
+this.pastTotalOperation = null;
 
-      this.presentTotal = Math.pow(this.presentTotal, 2);
+this.presentTotal = null;
 
+}
+Allclear(){
+this.click.emit(this.output);
+this.pastTotal = null;
 
-      this.lastOperation =
-        this.SQUARE;
+this.presentTotal = null;
 
-    }
-    else if (this.pastOperation == this.CUBE) {
+this.pastTotalOperation = null;
+this.symbol="";
+this.setOutput("0");
 
-      this.presentTotal = Math.pow(this.presentTotal, 3);
+}
 
+clear(){
+ 
+this.click.emit(this.output);
+if(this.symbol){
+  this.symbol="";
+  
+}else if(!this.symbol){
+var str=this.output;
+this.output=str.slice(0,-1);
 
-      this.lastOperation =
-        this.CUBE;
 
-    }
-    else {
 
-      if (this.lastOperation) {
+this.presentTotal=this.toNumber(this.output);
+}
 
-        if (this.lastOperation == this.ADD) {
+}
 
-          if (this.presentTotal) {
+setOutput(outputString) {
+this.click.emit(this.output);
+this.output = outputString;
 
-            this.presentTotal += this.lastValue;
+this.newNumber = 
+true;
 
-          } else {
 
-            this.presentTotal =
-              0;
+}
 
-          }
+toNumber(numberString) {
+this.click.emit(this.output);
+var result =0;
 
-        } else if (this.lastOperation ==
-          this.SUBTRACT) {
+if(numberString) {
 
-          if (this.presentTotal) {
+result = numberString*1;
 
-            this.presentTotal -= this.lastValue;
+}
 
-          } else {
+return result;
 
-            this.presentTotal =
-              0;
+}
 
-          }
-
-        }
-
-        else if (this.lastOperation ==
-          this.MULTIPLY) {
-
-          if (this.presentTotal) {
-
-            this.presentTotal *= this.lastValue;
-
-          } else {
-
-            this.presentTotal = 0;
-
-          }
-
-        }
-
-        else if (this.lastOperation ==
-          this.DIVIDE) {
-
-          if (this.presentTotal) {
-
-            this.presentTotal /= this.lastValue;
-
-          } else {
-
-            this.presentTotal = 0;
-
-          }
-
-        }
-
-        else if (this.lastOperation ==
-          this.MODULO) {
-
-          if (this.presentTotal) {
-            this.presentTotal %= this.lastValue;
-
-          } else {
-
-            this.presentTotal = 0;
-
-          }
-
-
-        }
-        else if (this.lastOperation ==
-          this.POW) {
-
-          if (this.presentTotal) {
-            this.presentTotal = Math.pow(this.presentTotal, this.lastValue);
-
-          } else {
-
-            this.presentTotal = 0;
-
-          }
-
-
-        }
-        else if (this.lastOperation ==
-          this.ROOT) {
-
-          if (this.presentTotal) {
-            this.presentTotal = Math.sqrt(this.lastValue);
-
-          } else {
-
-            this.presentTotal = 0;
-
-          }
-
-
-        }
-        else if (this.lastOperation ==
-          this.SQUARE) {
-
-          if (this.presentTotal) {
-            this.presentTotal = Math.pow(this.presentTotal, 2);
-
-          } else {
-
-            this.presentTotal = 0;
-
-          }
-
-
-        }
-        else if (this.lastOperation ==
-          this.CUBE) {
-
-          if (this.presentTotal) {
-            this.presentTotal = Math.pow(this.presentTotal, 3);
-
-          } else {
-
-            this.presentTotal = 0;
-
-          }
-
-
-        }
-      } else {
-
-        this.presentTotal = 0;
-
-      }
-
-    }
-
-    this.setOutput(this.presentTotal);
-
-    this.setsymbol("");
-
-    this.pastOperation =
-      null;
-
-    this.pastTotal =
-      null;
-
-  }
-
-
-  Allclear() {
-    this.click.emit(this.output);
-    this.presentTotal =
-      null;
-
-    this.pastTotal =
-      null;
-
-    this.pastOperation =
-      null;
-    this.symbol = "";
-    this.setOutput("0");
-
-  }
-
-  clear() {
-
-    this.click.emit(this.output);
-    if (this.symbol) {
-      this.symbol = "";
-
-    } else if (!this.symbol) {
-      var str = this.output;
-      this.output = str.slice(0, -1);
-
-
-
-      this.pastTotal = this.toNumber(this.output);
-    }
-
-  }
-
-  setOutput(outputString) {
-    this.click.emit(this.output);
-    this.output = outputString;
-
-    this.newNumber =
-      true;
-
-
-  }
-
-  toNumber(numberString) {
-    this.click.emit(this.output);
-    let result = 0;
-
-    if (numberString) {
-
-      result = numberString * 1;
-
-    }
-
-    return result;
-
-  }
 }
